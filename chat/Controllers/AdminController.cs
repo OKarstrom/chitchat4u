79f05da781936225670ba49336728f9cbbfb840f
ChristianZaar <christian.zaar.82@gmail.com>
2020-03-23 10:05:02 +0100

    Fixed admin leak

--- chat/Controllers/AdminController.cs
+++ chat/Controllers/AdminController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using chat.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace chat.Controllers
 {
+    [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
         private UserManager<ApplicationUser> userManager;
